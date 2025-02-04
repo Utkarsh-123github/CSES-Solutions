b0cf725ab449780b23d1dc7f5a80b14f6df9111d
Utkarsh Tiwari <utk0703@gmail.com>
2025-02-04 08:17:55 +0530

    Number of paths with and without obstacles

---
+++ Dynamic Programming/07_number_of_paths.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mod = 1e9+7;
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    vector<vector<char>>arr(n,vector<char>(n));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    vector<vector<long long int>>dp(n,vector<long long int>(n,0));
+    for(int i=n-1;i>=0;i--){
+        for(int j=n-1;j>=0;j--){
+            if(i==n-1 && j==n-1){
+                dp[i][j] = 1;
+            }
+            else{
+                int down = 0;
+                int right = 0;
+                if(i<n-1){
+                    down = (down + dp[i+1][j])%mod;
+                }
+                if(j<n-1){
+                    right = (right + dp[i][j+1])%mod;
+                }
+                dp[i][j] = (down + right)%mod;
+            }
+        }
+    }
+    cout<<dp[0][0];
+
+    
+    return 0;
+}
